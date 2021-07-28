6cbdf72817de24ddc4837d97caf69ea071acbca3
FrancescoCina <francesco.cina04@gmail.com>
2021-07-28 16:51:29 +0200

    Faccio vedere la foto con classe active attraverso specifica funzione

--- js/script.js
+++ js/script.js
@@ -4,5 +4,23 @@ Vue.config.devtools = true;
 
 const root = new Vue({
     el: "#root",
-    data:,
+    data: {
+        images: [
+            "./img/image1.jpg",
+            "./img/image2.jpg",
+            "./img/image3.jpg",
+            "./img/image4.jpg",
+        ],
+        currentIndex: 0,
+
+    },
+    methods: {
+        showImg(index) {
+            if (this.currentIndex === index) {
+                return 'active';
+            } else {
+                return '';
+            }
+        }
+    },
 })
