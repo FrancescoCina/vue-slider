5ca17c6033c99bc659bbfa0f030ca0fbbb808f67
FrancescoCina <francesco.cina04@gmail.com>
2021-07-28 17:16:31 +0200

    Inserito v-if per far scomparire le frecce a 0 e a <images.length

--- js/script.js
+++ js/script.js
@@ -5,13 +5,14 @@ Vue.config.devtools = true;
 const root = new Vue({
     el: "#root",
     data: {
+        currentIndex: 0,
         images: [
             "./img/image1.jpg",
             "./img/image2.jpg",
             "./img/image3.jpg",
             "./img/image4.jpg",
         ],
-        currentIndex: 0,
+
 
     },
     methods: {
@@ -21,6 +22,17 @@ const root = new Vue({
             } else {
                 return '';
             }
+        },
+        increase() {
+
+
+            return this.currentIndex++;
+
+
+        },
+        decrease() {
+            return this.currentIndex--;
         }
     },
 })
+
